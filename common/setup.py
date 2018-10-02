d042583fc5875b62fef35b897c0af64a8d256963
Manoj Patel <manojk.patel@gmail.com>
2018-10-02 18:08:23 +0530

    Second commit

--- common/setup.py
+++ common/setup.py
@@ -1,1 +1,5 @@
+import os
 
+def download_github_code(path):
+  filename = path.rsplit("/")[-1]
+  os.system("wget https://raw.githubusercontent.com/manojpatel/NLP/master/{} -O {}".format(path, filename))
